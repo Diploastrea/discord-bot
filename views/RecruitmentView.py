bd1176b9fc165bc49a5718d8b82cdd2aa6d390fe
Viet Duc Ho <23vietduc@gmail.com>
2025-03-28 19:59:38 +1100

    Fix perms issues for application channels

--- views/RecruitmentView.py
+++ views/RecruitmentView.py
@@ -2,10 +2,10 @@ import discord
 
 from PIL import Image
 from io import BytesIO
-from discord import ButtonStyle, PermissionOverwrite, File, Embed, Colour
+from discord import ButtonStyle, File, Embed, Colour
 from discord.utils import get
 
-from constants import RECRUITMENT_CATEGORY_ID, LEADERSHIP_ROLE_ID
+from constants import RECRUITMENT_CATEGORY_ID
 from views.ApplicationView import ApplicationView
 
 
@@ -20,20 +20,12 @@ class RecruitmentView(discord.ui.View):
         guild = interaction.guild
         user = interaction.user
         recruit_category = get(guild.categories, id=RECRUITMENT_CATEGORY_ID)
-        admin_role = guild.get_role(LEADERSHIP_ROLE_ID)
-
-        overwrites = {
-            guild.default_role: PermissionOverwrite(read_messages=False),
-            guild.me: PermissionOverwrite(read_messages=True, send_messages=True),
-            user: PermissionOverwrite(read_messages=True, send_messages=True),
-            admin_role: PermissionOverwrite(read_messages=True, send_messages=True)
-        }
 
         app_exists = f'{user.name}-application' in [app.name for app in recruit_category.channels[4:]]
         if not app_exists:
             await interaction.response.send_message("Your application has been created successfully.", ephemeral=True)
-            channel = await guild.create_text_channel(f'{user.name} application', category=recruit_category,
-                                                      overwrites=overwrites)
+            channel = await guild.create_text_channel(f'{user.name} application', category=recruit_category)
+            await channel.edit(synced_permissions=True)
             message = f'{user.mention} Welcome! Please give yourself a short introduction and post screenshots of your ' \
                       'box (sorted by factions), pets, tree, collections and rank plates as shown in example collage ' \
                       'below. If you have a preference, please specify which guild branch you\'d like to join.'
