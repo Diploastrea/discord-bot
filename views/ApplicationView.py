2284c3cf989ca6d09824bcbac8f9d06f14687a10
Viet Duc Ho <23vietduc@gmail.com>
2025-03-30 21:58:58 +1100

    Change close app message to ephemeral

--- views/ApplicationView.py
+++ views/ApplicationView.py
@@ -12,11 +12,8 @@ class ApplicationView(discord.ui.View):
 
     @discord.ui.button(label='Close application', custom_id='azor:button-close', style=ButtonStyle.danger)
     async def close(self, interaction, button):
-        app_channel = self.client.get_channel(interaction.channel.id)
         embed_message = 'Are you sure you want to close the application? This action will delete the channel and ' \
                         'cannot be reversed.'
         embed = Embed(description=embed_message, colour=Colour.dark_green())
 
-        message = await app_channel.send(embed=embed)
-        await message.edit(view=ConfirmView())
-        await interaction.response.defer()
+        await interaction.response.send_message(embed=embed, view=ConfirmView(), ephemeral=True)
